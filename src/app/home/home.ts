88172ca72c045e17c0826213b532d16ceb34e30a
Sanskar-12 <sanskarv2004@gmail.com>
2026-01-24 17:05:57 +0530

    Fixed all the jsons imageUrl and made product slider

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { MainCarousel } from '../main-carousel/main-carousel';
 import { ProductSlider } from './product-slider/product-slider';
 import { menJeans } from '../../Data/Men/men_jeans';
+import { gounsPage1 } from '../../Data/Gouns/gouns';
+import { lengha_page1 } from '../../Data/LehangaCholi/lehangaCholi';
+import { mens_kurta } from '../../Data/MensKurta/mensKurta';
+import { mensShoesPage1 } from '../../Data/Shoes/menShoes';
 
 @Component({
   selector: 'app-home',
@@ -11,8 +15,16 @@ import { menJeans } from '../../Data/Men/men_jeans';
 })
 export class Home {
   menJeans: any;
+  womenGouns: any;
+  lehangaCholi: any;
+  mensKurta: any;
+  menShoes: any;
 
   ngOnInit() {
-    this.menJeans = menJeans.slice(0, 5);
+    this.menJeans = menJeans.slice(0, 4);
+    this.womenGouns = gounsPage1.slice(0, 4);
+    this.lehangaCholi = lengha_page1.slice(0, 4);
+    this.mensKurta = mens_kurta.slice(0, 4);
+    this.menShoes = mensShoesPage1.slice(0, 4);
   }
 }
