e28d88696ef83092fa943a7eca3a58e3bcf93828
Sanskar-12 <sanskarv2004@gmail.com>
2026-01-26 18:32:54 +0530

    Made Navbar and navbar content design

--- src/app/navbar/navbar-content/navbar-content.ts
+++ src/app/navbar/navbar-content/navbar-content.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { navigation } from '../../../Data/navigation';
+import { MatAnchor } from '@angular/material/button';
 
 @Component({
   selector: 'app-navbar-content',
-  imports: [],
+  imports: [MatAnchor],
   templateUrl: './navbar-content.html',
   styleUrl: './navbar-content.scss',
 })
 export class NavbarContent {
+  category: any;
+  @Input() selectedSection: any;
 
+  ngOnInit() {
+    this.category = navigation.categories;
+  }
 }
--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,17 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterLink } from '@angular/router';
+import { NavbarContent } from './navbar-content/navbar-content';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [RouterLink, MatButtonModule, MatIconModule, MatMenuModule, NavbarContent],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
-  openNavbarContent(menu: any) {}
+  isNavbarContentOpen: any;
+  currentSection: any;
+
+  openNavbarContent(section: any) {
+    this.isNavbarContentOpen = true;
+    this.currentSection = section;
+  }
+
+  closeNavbarContent() {
+    this.isNavbarContentOpen = false;
+  }
 
   navigateTo(path: any) {}
+
+  @HostListener('document:click', [`$event`])
+  onDocumentClick(event: MouseEvent) {
+    const modalContainer = document.querySelector('.modal-container');
+    const openButtons = document.querySelectorAll('.open-buttons');
+
+    let clickInsideButton = false;
+
+    openButtons.forEach((button: Element) => {
+      if (button.contains(event.target as Node)) {
+        clickInsideButton = true;
+      }
+    });
+
+    if (modalContainer && !clickInsideButton && this.isNavbarContentOpen) {
+      this.closeNavbarContent();
+    }
+  }
 }
