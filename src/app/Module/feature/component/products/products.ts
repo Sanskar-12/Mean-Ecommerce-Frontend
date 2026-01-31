be8dcdc9b8a2062e0c687a2116e856489c9517f7
Sanskar-12 <sanskarv2004@gmail.com>
2026-01-31 12:07:37 +0530

    Added the logic for filtering the products

--- src/app/Module/feature/component/products/products.ts
+++ src/app/Module/feature/component/products/products.ts
@@ -9,6 +9,7 @@ import { MatRadioButton } from '@angular/material/radio';
 import { menJeans } from '../../../../../Data/Men/men_jeans';
 import { ProductCard } from '../../../shared/component/product-card/product-card';
 import { SharedModule } from '../../../shared/shared-module';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -29,6 +30,54 @@ export class Products {
   filterData: any;
   singleFilterData: any;
   menPants: any;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) {}
+
+  // for checkbox filter
+  handleMultipleSelectFilter(value: string, sectionId: string) {
+    // taking query params from the path
+    const queryParams = { ...this.activatedRoute.snapshot.queryParams };
+
+    // making the array of filter values using section Id
+    const filterValues = queryParams[sectionId] ? queryParams[sectionId].split(',') : [];
+
+    // finding the index of that value in filterValues array
+    const valueIndex = filterValues.indexOf(value);
+
+    if (valueIndex !== -1) {
+      // if the value is present then remove that value from the array
+      filterValues.splice(valueIndex, 1);
+    } else {
+      // if not present then push the value in the array
+      filterValues.push(value);
+    }
+
+    if (filterValues.length > 0) {
+      // if array is present then join it and add it inside query params
+      queryParams[sectionId] = filterValues.join(',');
+    } else {
+      // if not then delete query params
+      delete queryParams[sectionId];
+    }
+
+    // navigate to that filter route
+    this.router.navigate([], { queryParams });
+  }
+
+  // for radio filter
+  handleSingleSelectFilter(value: string, sectionId: string) {
+    // taking query params from the path
+    const queryParams = { ...this.activatedRoute.snapshot.queryParams };
+
+    // set the query params to that value
+    queryParams[sectionId] = value;
+
+    // navigate to that filter route
+    this.router.navigate([], { queryParams });
+  }
 
   ngOnInit() {
     this.filterData = filters;
