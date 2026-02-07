7d6a937886aa5ea9e8964bed3308f3d9848346d4
Sanskar-12 <sanskarv2004@gmail.com>
2026-02-07 14:58:40 +0530

    Created Cart module

--- src/app/Module/feature/component/cart/cart.ts
+++ src/app/Module/feature/component/cart/cart.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { CartItems } from '../../../shared/component/cart-items/cart-items';
 import { MatDivider } from '@angular/material/divider';
+import { Router } from '@angular/router';
+import { MatAnchor } from '@angular/material/button';
 
 @Component({
   selector: 'app-cart',
-  imports: [CartItems, MatDivider],
+  imports: [CartItems, MatDivider, MatAnchor],
   templateUrl: './cart.html',
   styleUrl: './cart.scss',
 })
 export class Cart {
   cart = [1, 1, 1];
+
+  constructor(private router: Router) {}
+
+  navigateToCheckout() {
+    this.router.navigate(['/checkout']);
+  }
 }
