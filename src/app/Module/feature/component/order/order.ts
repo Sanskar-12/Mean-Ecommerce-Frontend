28f50d34020200685880a31069678cdc4b586092
Sanskar-12 <sanskarv2004@gmail.com>
2026-02-14 12:14:15 +0530

    Made my orders page and order card

--- src/app/Module/feature/component/order/order.ts
+++ src/app/Module/feature/component/order/order.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { OrderCard } from './order-card/order-card';
 
 @Component({
   selector: 'app-order',
-  imports: [],
+  imports: [MatCheckbox, OrderCard],
   templateUrl: './order.html',
   styleUrl: './order.scss',
 })
 export class Order {
-
+  orderFilter = [
+    { value: 'on_the_way', label: 'On the Way' },
+    { value: 'delivered', label: 'Delivered' },
+    { value: 'cancelled', label: 'Cancelled' },
+    { value: 'returned', label: 'Returned' },
+  ];
+  orders = [
+    [1, 1],
+    [1, 1],
+  ];
 }
